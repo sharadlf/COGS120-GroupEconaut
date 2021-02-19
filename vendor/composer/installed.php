cb7fc885eb2ce5a9522d19f3566a1b241aeae7da
Sharad Bhagwat <sharadLF@gmail.com>
2021-02-18 21:41:03 -0800

    Manual deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cbba6455d42874feb066c1ab9849f409e6dc9734',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cbba6455d42874feb066c1ab9849f409e6dc9734',
+    ),
+  ),
+);
