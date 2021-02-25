cadd8c210f0566b9c5765a9f2cea0005959abfff
Ishaan Bose <ibose@ucsd.edu>
2021-02-24 17:13:02 -0800

    Add Cards for Cars, and Functions.js for click listeners

--- routes/comparison.js
+++ routes/comparison.js
@@ -1,5 +1,37 @@
 exports.view = function(req, res){
     res.render("comparison", {
-        
+        "BMW":[
+            {
+              "image":"BMW Card.svg"
+            }
+          ],
+          "Toyota":[
+            {
+              "image":"Prius Card.svg"
+            },
+      
+            {
+              "image":"Camry Card.svg"
+            }
+          ],
+          
+          "Honda":[
+            {
+                "image":"Civic Card.svg"
+            }
+          ],
+      
+          "Jeep":[
+            {
+                "image":"Jeep Card.svg"
+            }
+          ],
+      
+          "Tesla":[
+            {
+                "image":"Tesla Card.svg"
+            }
+          ]
+          
     });
 }
