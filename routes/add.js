390643357a5c64c7f7a93cc5a38f27126806642d
Ishaan Bose <ibose@ucsd.edu>
2021-03-11 22:04:36 -0800

    Finished Add functionality, added logo to be worked on

--- routes/add.js
+++ routes/add.js
@@ -1,26 +1,32 @@
+let cars = require('../cars.json');
 let carlist = require('../carlist.json');
+
 
 
 exports.addCar = function(request, response) {   
 	// Your code goes here
-
-	var newCar = [{
-		"brand": request.query.brand,
-		"model": request.query.model,
-		"image": "https://www.fillmurray.com/640/360"
-	}]
+	var length = carlist.cars.length;
+	//console.log("LENGTH IS: " + length);
+	var newCar = { 
+		"26" : {
+			"brand": request.query.brand,
+			"model": request.query.model,
+			"id": length,
+			"image": "https://www.fillmurray.com/640/360"
+		}}
+	//console.log(carlist.cars.length);
 	
     //console.log(newCar);
-    console.log(carlist);
+   // console.log(carlist.cars);
 
     carlist.cars.push(newCar);
 
     console.log("DIVIDER!!!!!")
-    console.log(carlist);
+    console.log(carlist.cars);
 
 
 
-	response.render('index',carlist); 
+	response.render('index',cars); 
 	
 //	carlist.cars.push(newCar);
 
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@
  */
 
 let cars = require('../cars.json');
+let carlist = require('../carlist.json');
+
 
 //console.log(cars);
 
@@ -11,3 +13,32 @@ exports.view = function(req, res){
   res.render("index", cars);
 };
 
+/*
+exports.addCar = function(request, response) {   
+	// Your code goes here
+	//var length = carlist.cars.length;
+	//console.log("LENGTH IS: " + length);
+	var newCar = { 
+		"26" : {
+			"brand": request.query.brand,
+			"model": request.query.model,
+			"id": length,
+			"image": "https://www.fillmurray.com/640/360"
+		}}
+	//console.log(carlist.cars.length);
+	
+    //console.log(newCar);
+   // console.log(carlist.cars);
+
+    carlist.cars.push(newCar);
+
+    console.log("DIVIDER!!!!!")
+    console.log(carlist.cars);
+
+
+
+	response.render('index',carlist); 
+	
+//	carlist.cars.push(newCar);
+
+}*/
